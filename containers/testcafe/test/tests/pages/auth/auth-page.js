f806da47472c419ee394c4de340fd652c76756ad
Soushi Nozawa <n.soushi@gmail.com>
2017-12-07 16:06:10 +0900

    Update auth-page.js

--- containers/testcafe/test/tests/pages/auth/auth-page.js
+++ containers/testcafe/test/tests/pages/auth/auth-page.js
@@ -5,7 +5,7 @@ import { Selector } from 'testcafe';
 export default class AuthPage {
     constructor () {
 
-        this.url = 'https://freshlive.tv/auth/fresh_id';
+        this.url = 'TEST_URL';
 
         this.idInput = Selector('#user_id');
         this.pwInput = Selector('#password');
